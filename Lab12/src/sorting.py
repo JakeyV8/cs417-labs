06dc368a3327f64424cc04654cc86992e29dc67b
JakeyV8 <jakeyv8@gmail.com>
2026-03-05 19:50:26 +0000

    Lab 12: Implement partition for quicksort

--- Lab12/src/sorting.py
+++ Lab12/src/sorting.py
@@ -144,9 +144,7 @@ def merge_sort(a_list):
         a_list[k] = right[j]
         j+= 1
         k+= 1
-
-        
-
+ 
     return a_list
 
 
@@ -181,7 +179,25 @@ def _partition(a_list, first, last):
     Returns:
         The index where the pivot ended up (the split point).
     """
-    pass  # TODO: implement this
+    pivot_value = a_list[first]
+    left_mark = first+1
+    right_mark = last
+    is_Done = False
+    while not is_Done:
+        while left_mark <= right_mark and a_list[left_mark] <= pivot_value:
+            left_mark += 1
+        while left_mark <= right_mark and a_list[right_mark] >= pivot_value:
+            right_mark -= 1
+        if right_mark < left_mark:
+            is_Done = True
+        else:
+            temp = a_list[left_mark]
+            a_list[left_mark] = a_list[right_mark]
+            a_list[right_mark] = temp
+    temp = a_list[first]
+    a_list[first] = a_list[right_mark]
+    a_list[right_mark] = temp
+    return right_mark
 
 
 def _quick_sort_helper(a_list, first, last):

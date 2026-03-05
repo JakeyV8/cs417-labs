52623ae6f66b9cf69e6f162f43cc19a63be8f5cc
JakeyV8 <jakeyv8@gmail.com>
2026-03-05 19:35:42 +0000

    Lab 12: Implement merge step for merge sort

--- Lab12/src/sorting.py
+++ Lab12/src/sorting.py
@@ -126,8 +126,26 @@ def merge_sort(a_list):
     # Combine the sorted left and right halves back into a_list.
     # Initialize: i = 0, j = 0, k = 0
     # Then write the three while loops described above.
+    i = j = k = 0
+    while i < len(left) and j<len(right):
+        if left[i] >= right[j]:
+            a_list[k] = right[j]
+            j +=1
+            k += 1
+        else:
+            a_list[k] = left[i]
+            i+= 1
+            k+= 1
+    while i < len(left):
+        a_list[k] = left[i]
+        i+= 1
+        k+= 1
+    while j < len(right):
+        a_list[k] = right[j]
+        j+= 1
+        k+= 1
 
-    pass  # TODO: replace this with the merge logic
+        
 
     return a_list
 
